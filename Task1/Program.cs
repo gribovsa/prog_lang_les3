ff4b4f92ba5e1443ab85a5b74e6c6f4d39bf06a1
Gribov Sergey <gribovsa@list.ru>
2024-01-13 12:48:25 +0300

    add lesson3:task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b; //выбираем тип данных как более вместительный, чтобы результат был более вместительный double и отображал дробь
+    int denominate = c + d;
+    double result = numinator / denominate;
+    return result;
+}
+
+CalculateFormula(1, 2, 3, 4); //задаем аргументы (уже аргументы а не параметры)
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
